5a39eb76b18fb1f8fb54de7f2ee459fc1cae3751
aakashsr <aakashsri.dev@gmail.com>
2019-08-22 23:51:09 +0530

    Stated the entry file and output files and folder for the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,14 @@ module.exports = {
         filename: 'index_bundle.js'
     },
 
-    module: {
-    rules: [ 
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
-    ]
-  },
+//     module: {
+//     rules
+//       { test: /\.(js)$/, use: 'babel-loader' },
+//       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+//     ]
+//   },
 
-  plugins: [new HtmlWebpackPlugin({
-      template: 'app/index.html'
-  })]
+//   plugins: [new HtmlWebpackPlugin({
+//       template: 'app/index.html'
+//   })]
 }
