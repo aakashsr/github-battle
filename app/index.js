cea8bd154e749471e6103a1e96a04c6b22735024
aakashsr <aakashsri.dev@gmail.com>
2019-08-23 12:31:35 +0530

    Added HtmlWebpackPlugin , added script to our package.json to run webpack-dev-server every time we make any changes to our file

--- app/index.js
+++ app/index.js
@@ -1,18 +1,11 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-require('./index.css');
+var React = require("react");
+var ReactDOM = require("react-dom");
+require("./index.css");
 
 class App extends React.Component {
-    render(){
-        return(
-            <div>
-                Hello World!
-            </div>
-        )
-    }
+  render() {
+    return <div>Hello Aakash!</div>;
+  }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById("app"));
--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,20 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-  entry: './app/index.js',
+  entry: "./app/index.js",
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: { 'style-loader', 'css-loader' }}
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }
     ]
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
-  }
-}
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      template: "app/index.html"
+    })
+  ]
+};
