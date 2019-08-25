078aeade799c590dc980ab51ba5723aa543e1aaa
aakashsr <aakashsri.dev@gmail.com>
2019-08-25 22:05:54 +0530

    Moved Badge into it's own component

--- app/index.js
+++ app/index.js
@@ -2,19 +2,7 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 require("./index.css");
 
-class Badge extends React.Component {
-  render() {
-    const { img, name, username } = this.props;
-    return (
-      <div>
-        <h1>Badge 1</h1>
-        <img src={img} alt="" />
-        <h2>Name: {name}</h2>
-        <h3>Username: {username}</h3>
-      </div>
-    );
-  }
-}
+
 
 ReactDOM.render(
   <Badge
