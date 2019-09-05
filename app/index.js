21fb31e6ce28a7f702808c0373d208cada70d1c2
aakashsr <aakashsri.dev@gmail.com>
2019-09-05 21:29:20 +0530

    Added the css file into the index.js file

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Popular from "./components/Popular";
+import './index.css';
 
 class App extends React.Component {
   render() {
