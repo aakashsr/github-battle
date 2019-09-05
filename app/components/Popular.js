8ba56429664022e97a9255122e900916e6da29c4
aakashsr <aakashsri.dev@gmail.com>
2019-09-05 21:16:50 +0530

    Added the languages array

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
 
 export default class Popular extends Component {
+
+
     render() {
+
+        const languages = ['All','JavaScript','Ruby','Java','CSS','Python'];
         return (
-          <ul>
+          <ul class='container'>
             <li>POPULAR</li>
           </ul>
         );
