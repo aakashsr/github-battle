bcfaa3c4e47695594a377f6f78167efd156b6210
aakashsr <aakashsri.dev@gmail.com>
2019-09-27 11:43:33 +0530

    using NavLink instead of Link as NavLink will allow us to pass some extra props to our link

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeConsumer } from "../contexts/theme";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const activeStyle = {
   color: "rgb(187, 46, 31)"
@@ -13,14 +13,23 @@ export default function Nav() {
         <nav className="row space-between">
           <ul className="row nav">
             <li>
-              <Link to="/" className="nav-link">
+              <NavLink
+                to="/"
+                exact
+                activeStyle={activeStyle}
+                className="nav-link"
+              >
                 Popular
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/battle" className="nav-link">
+              <NavLink
+                to="/battle"
+                activeStyle={activeStyle}
+                className="nav-link"
+              >
                 Battle
-              </Link>
+              </NavLink>
             </li>
           </ul>
           <button
