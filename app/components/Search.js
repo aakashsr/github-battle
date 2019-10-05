ade853331f14230ee8d97bcf9330b77192000b1d
aakashsr <aakashsri.dev@gmail.com>
2019-10-05 23:09:03 +0530

    added the content in Search component

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,11 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { ThemeConsumer } from "../contexts/theme";
 
 export default class Search extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Search</h1>
+  state = {
+    userName: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      username: event.target.value
+    });
+  };
+  render() {
+    return (
+      <ThemeConsumer>
+        {({ theme }) => (
+          <div>
+            <h1 className="center-text header-lg">Instructions</h1>
+            <div className="row player-inputs">
+              <input
+                type="text"
+                id="username"
+                className={`input-${theme}`}
+                placeholder="github username"
+                autoComplete="off"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+              <button
+                className={`btn ${theme === "dark" ? "light-btn" : "dark-btn"}`}
+                type="submit"
+                disabled={!this.state.username}
+              >
+                Submit
+              </button>
             </div>
-        )
-    }
+          </div>
+        )}
+      </ThemeConsumer>
+    );
+  }
 }
