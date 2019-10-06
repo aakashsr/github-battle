cc6748f49dbaf6d677598862fe13b61ee5c66843
aakashsr <aakashsri.dev@gmail.com>
2019-10-06 23:58:38 +0530

    created a new component Birthday

---
+++ app/components/Birthday.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Birthday = () => {
+    return (
+        <div>
+            <h1>You birthday is 3 Sep.</h1>
+        </div>
+    );
+}
+
+export default Birthday;
